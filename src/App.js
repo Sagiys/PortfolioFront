8e558364fd3072791f5108427b6ccf55adcb33fe
Adam Hallmann <hallmann.adam00@gmail.com>
2019-12-06 22:28:32 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      Basic template, create react app with scss using webpack
+    </div>
+  );
+}
+
+export default App;
